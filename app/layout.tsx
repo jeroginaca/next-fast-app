79770a15788e2c048b3ac5b304d36509d19b6758
JeronimoGinaca <jeronimo.ginaca@globant.com>
2023-09-20 12:51:11 +0200

    metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,15 @@ import type { Metadata } from 'next'
 export const metadata: Metadata = {
   title: 'CodeHacker Academy',
   description: 'CodeHacker Academy Resources',
+  other: {
+    "theme-color": "#0d1117",
+    "color-scheme": "dark only",
+    "twitter:image": 'https://res.cloudinary.com/dr3cvyonp/image/upload/v1694865028/bg-contact_rigmdj.png',
+    "twitter:card": "summary_large_image",
+    "og:url": "codehackeracademy.com",
+    "og:image": 'https://res.cloudinary.com/dr3cvyonp/image/upload/v1694865028/bg-contact_rigmdj.png',
+    "og:type": "website",
+  }
 }
 
 export default function RootLayout({
